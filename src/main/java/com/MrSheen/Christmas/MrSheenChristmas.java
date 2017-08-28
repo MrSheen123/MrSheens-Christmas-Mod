b18337f5c9401ca1a9d394c32f24696e25531d73
MrSheen123 <s-mullen@outlook.com>
2017-08-28 20:43:04 +0100

    Adding

--- src/main/java/com/MrSheen/Christmas/MrSheenChristmas.java
+++ src/main/java/com/MrSheen/Christmas/MrSheenChristmas.java
@@ -1,5 +1,7 @@
 package com.MrSheen.Christmas;
 
+import com.MrSheen.Christmas.tab.CreativeChristmasTab;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -11,17 +13,21 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class MrSheenChristmas {
     public static final String MODID = "mrsheenchristmas";
     public static final String VERSION = "1.0";
-    public static final String NAME = "Mr Sheen's Christmas Mod"
+    public static final String NAME = "Mr Sheen's Christmas Mod";
+
+    @Mod.Instance
+    public static MrSheenChristmas instance;
+
+    public static CreativeChristmasTab tabChristmas;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-
+        tabChristmas = new CreativeChristmasTab(CreativeTabs.getNextID(), "tab_Christmas");
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        // some example code
-        System.out.println("DIRT BLOCK >> " + Blocks.DIRT.getUnlocalizedName());
+
     }
 
     @EventHandler
